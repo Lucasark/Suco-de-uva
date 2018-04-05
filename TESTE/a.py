e2034dff1cac631086c5517f9b795395b8962199
Lucasark <31750882+Lucasark@users.noreply.github.com>
2018-04-05 13:45:19 -0300

    Add files via upload

--- TESTE/a.py
+++ TESTE/a.py
@@ -8,14 +8,12 @@ print ("Ta pronto mane!")
 while 1:
 	connectionSocket, addr = serverSocket.accept()
 	connectionSocket.recv(1024)
-	connectionSocket.send('HTTP/1.0 200 0K\n')
-	connectionSocket.send('\n')
-	connectionSocket.send("""
+#	connectionSocket.send(b"HTTP/1.0 200 0K\n")
+	connectionSocket.send(b"""
 		<html>
 		<body>
 		<h1>Hello World</h1>
 		</body>
 		</html>
-
-	"""	)
+		""")
 	connectionSocket.close()
