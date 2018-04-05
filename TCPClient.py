1e1b7210aa15c9ebc7f44af36bc30269bb2aab55
Lucasark <31750882+Lucasark@users.noreply.github.com>
2018-04-05 08:05:04 -0300

    Finished
    Redes - 1.18

--- TCPClient.py
+++ TCPClient.py
@@ -1,17 +1,17 @@
 from socket import *
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
 a = 1
 while a:
+	serverName = 'localhost'
+	serverPort = 12000
+	clientSocket = socket(AF_INET, SOCK_STREAM)
 	clientSocket.connect((serverName,serverPort))
-	setence = input('Chega mais ai, patrao:').encode()
-	clientSocket.sendto(sentence)
-	modifiedMessage = clientSocket.recvfrom(1024)
-	print('Saindo de Jesus:'+modifiedMessage)
+	sentence = input('Chega mais ai, patrao:').encode()
+	clientSocket.send(sentence)
+	modifiedMessage = clientSocket.recv(1024)
+	print('Saindo de Jesus:', modifiedMessage)
+	clientSocket.close()
 	b = input('Denovo, irmao?')
 	if (b == 'Y' or b == 'y'):
 		a = 1
 	else:
 		a = 0
-clientSocket.close()
